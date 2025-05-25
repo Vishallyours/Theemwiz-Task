55d216921295fbf3f5800cb107c85ccd7edb697c
Vishal Sain <117062321+Vishallyours@users.noreply.github.com>
2025-05-25 01:12:20 +0000

    portfolio section start

--- theemwiz-task/src/App.jsx
+++ theemwiz-task/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import HeroSection from "./sections/HeroSection";
 import AboutSection from "./sections/AboutSection";
+import PortfolioSection from "./sections/PortfolioSection";
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
     <Navbar />
     <HeroSection />  
     <AboutSection />
+    <PortfolioSection />
     </>
   )
 }
---
+++ theemwiz-task/src/sections/PortfolioSection.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import SectionCard from '../components/SectionCard'
+
+function PortfolioSection() {
+    return (
+    <div className='bg-[#28293E] text-white font-epilogue w-full h-auto md:px-36 md:py-24'>
+        <div className='flex flex-col justify-center items-center'>
+            
+            <span className='tracking-widest text-xl text-rose-500'>PORTFOLIO</span>
+
+            <span className='flex font-extrabold leading-none md:text-7xl text-4xl '>Latest Work</span>
+        </div>
+    </div>
+    )
+}
+
+export default PortfolioSection
